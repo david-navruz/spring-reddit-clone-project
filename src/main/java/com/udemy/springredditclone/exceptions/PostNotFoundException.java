66492cfedda22e5ff6936fa9dde62c200f4a4941
david-navruz <selcuk-nevruz@hotmail.com>
2022-09-15 22:23:13 +0300

    First commit

---
+++ src/main/java/com/udemy/springredditclone/exceptions/PostNotFoundException.java
@@ -0,0 +1,8 @@
+package com.udemy.springredditclone.exceptions;
+
+public class PostNotFoundException extends RuntimeException {
+
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+}
