90e52fb679487c4d39d90f579cc81c36336be6f5
david-navruz <selcuk-nevruz@hotmail.com>
2022-09-15 23:48:35 +0300

    Updating the project : repository

---
+++ src/main/java/com/udemy/springredditclone/repository/PostRepository.java
@@ -0,0 +1,18 @@
+package com.udemy.springredditclone.repository;
+
+import com.udemy.springredditclone.model.Post;
+import com.udemy.springredditclone.model.Subreddit;
+import com.udemy.springredditclone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllBySubreddit(Subreddit subreddit);
+
+    List<Post> findByUser(User user);
+
+}
